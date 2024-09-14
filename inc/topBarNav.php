e137379aa7c24f6d6bfcd58eff1a4642113cbd6d
darwinweng2002 <vdarwin860@gmail.com>
2024-09-14 12:14:37 +0800

    up

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -8,7 +8,7 @@
     color: #012970;
 }
 /* Make dropdown menu visible and regular nav links hidden on smaller screens */
-@media (min-width: 512px) {
+@media (min-width: 412px) {
     /* Show toggle button on smaller screens */
     #navbar-toggler {
         display: block;
