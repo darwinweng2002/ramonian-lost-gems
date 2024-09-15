bd300fb672e5cbe38554cc3b69bcc261013b1913
darwinweng2002 <vdarwin860@gmail.com>
2024-09-15 10:33:31 +0800

    up

--- itemss/published_items.php
+++ itemss/published_items.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 // Database connection
-$conn = new mysqli('localhost', 'u450897284_root', 'Lfisgemsdb1234', 'u450897284_Ifis_db'); // Replace with your actual DB connection details
+$conn = new mysqli('localhost','u450897284_root', 'Lfisgemsdb1234', 'u450897284_lfis_db'); // Replace with your actual DB connection details
 
 // Check connection
 if ($conn->connect_error) {
