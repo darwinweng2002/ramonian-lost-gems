f87f2337b689ccabcef0e60520d46651e9df6b30
darwinweng2002 <vdarwin860@gmail.com>
2024-09-15 10:23:41 +0800

    up

--- admin/messages/reported_items.php
+++ admin/messages/reported_items.php
@@ -2,7 +2,7 @@
 include '../../config.php';
 
 // Database connection
-$conn = new mysqli('localhost', 'u450897284_root', 'Lfisgemsdb1234', 'u450897284_lfis_db'); // Update with your DB credentials
+$conn = new mysqli('localhost', 'root', '1234', 'lost_db'); // Update with your DB credentials
 
 // Check connection
 if ($conn->connect_error) {
@@ -79,7 +79,7 @@ $result = $conn->query($sql);
                         echo "<td>" . htmlspecialchars($row['first_name']) . "</td>";
                         echo "<td>" . htmlspecialchars($row['college']) . "</td>";
                         echo "<td>" . htmlspecialchars($row['time_found']) . "</td>";
-                        echo "<td><a href='https://ramonianlostgems.com/view_reported_item.php?id=" . htmlspecialchars($row['id']) . "' class='action-btn'>View</a></td>";
+                        echo "<td><a href='https://ramonianlostgems.com/admin/messages/view_reported_item.php?id=" . htmlspecialchars($row['id']) . "' class='action-btn'>View</a></td>";
                         echo "</tr>";
                     }
                 } else {
