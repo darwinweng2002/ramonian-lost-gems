dbce5a72b6ae1538aca7b3e0805401dbca0ffbf4
darwinweng2002 <vdarwin860@gmail.com>
2024-09-18 23:40:44 +0800

    up

--- itemss/items.php
+++ itemss/items.php
@@ -8,7 +8,7 @@
     }
 
     // Database connection
-    $conn = new mysqli('localhost', 'root', '1234', 'lost_db'); 
+    $conn = new mysqli('localhost', 'u450897284_root', 'Lfisgemsdb1234', 'u450897284_lfis_db');
 
     // Check connection
     if ($conn->connect_error) {
