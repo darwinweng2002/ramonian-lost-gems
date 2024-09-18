9e478de8faac02b6334f6cc7ebe617f509c5fa97
darwinweng2002 <vdarwin860@gmail.com>
2024-09-18 20:06:40 +0800

    up

--- admin/missing_items/view_missing.php
+++ admin/missing_items/view_missing.php
@@ -1,11 +1,5 @@
 <?php
 include '../config.php';
-// Check if user is logged in
-if (!isset($_SESSION['user_id'])) {
-    // Redirect to login page if not logged in
-    header('Location: login.php'); // Adjust this path if necessary
-    exit();
-}
 
 // Database connection
 $conn = new mysqli('localhost', 'u450897284_root', 'Lfisgemsdb1234', 'u450897284_lfis_db'); // Replace with your actual DB connection details
