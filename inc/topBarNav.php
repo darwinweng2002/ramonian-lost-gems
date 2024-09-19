1472efe13431b78189f94d479bbcaa4377024bdc
darwinweng2002 <vdarwin860@gmail.com>
2024-09-20 01:57:34 +0800

    up

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -241,7 +241,7 @@
     top: 0; /* Adjust to align with your design */
     background-color: #3498db; /* Professional color */
     color: white;
-    padding: 12px 30px;
+    padding: 14px 30px;
     border: none;
     border-radius: 2px;
     cursor: pointer;
