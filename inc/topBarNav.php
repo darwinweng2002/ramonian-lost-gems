cdbf195fa00a7ac7b12aa6a55303d9b65a40c66d
darwinweng2002 <vdarwin860@gmail.com>
2024-09-20 21:05:06 +0800

    up

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -270,7 +270,7 @@
 .ramon h1 {
     margin: 0;
     font-size: 24px;
-    color: #ccc; /* Adjust color for visibility */
+    color: #00274D; /* Adjust color for visibility */
     text-align: center;
 }
 /* Responsive adjustments for sidebar */
