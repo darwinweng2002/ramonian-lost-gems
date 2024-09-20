25873b0dfa40b0de1d6f1e69af65ca2f215a67ee
darwinweng2002 <vdarwin860@gmail.com>
2024-09-20 22:07:27 +0800

    up

--- inc/topBarNavs.php
+++ inc/topBarNavs.php
@@ -69,7 +69,7 @@
     top: 0; /* Adjust to align with your design */
     background-color: #3498db; /* Professional color */
     color: white;
-    padding: 17px 30px;
+    padding: 19px 30px;
     border: none;
     border-radius: 2px;
     cursor: pointer;
