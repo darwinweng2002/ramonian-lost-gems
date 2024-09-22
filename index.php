684a09d22d23a732a6380db6a0ed45814b82749d
darwinweng2002 <vdarwin860@gmail.com>
2024-09-23 02:10:13 +0800

    up

--- index.php
+++ index.php
@@ -188,26 +188,7 @@ if (isset($_POST['guest_login'])) {
                   </div>
                 </div>
               </div>
-              <footer>
-                <div class="container text-center py-4">
-                  <!-- Copyright Section -->
-                  <div class="copyright mb-2">
-                    &copy; <strong><span>Ramonian LostGems</span></strong>. All Rights Reserved
-                  </div>
-                  <!-- Credits Section -->
-                  <div class="credits">
-                    <p>
-                      <a href="https://ramonianlostgems.com/">ramonianlostgems.com</a>
-                    </p>
-                  </div>
-                  <!-- Logo Section -->
-                  <div class="logo mb-2">
-                    <a href="<?= base_url ?>">
-                      <img style="height: 55px; width: 55px;" src="<?= validate_image($_settings->info('logo')) ?>" alt="System Logo">
-                    </a>
-                  </div>
-                </div>
-              </footer>
+          
             </div>
           </div>
         </div>
@@ -297,6 +278,6 @@ if (isset($_POST['guest_login'])) {
   });
 </script>
 
- 
+<?php require_once('inc/footer.php') ?>
 </body>
 </html>
