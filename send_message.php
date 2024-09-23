5ecc08a2ab31b9b98b88e162a22c13f328e4717b
darwinweng2002 <vdarwin860@gmail.com>
2024-09-24 03:03:07 +0800

    up

--- send_message.php
+++ send_message.php
@@ -211,7 +211,7 @@ if (isset($_SESSION['user_id'])) {
         <form action="send_message.php" method="post" enctype="multipart/form-data" class="message-form">
         <label for="founder_name">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>
-                </svg> Item Name:
+                </svg> Founder Name:
             </label>
             <input type="text" name="founder_name" id="founder_name" placeholder="Enter founder name" required>
         <label for="title">
