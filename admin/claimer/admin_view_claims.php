08a0494ffaf3553d8dc0424cc9fcc73242ee3a57
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 00:44:21 +0800

    up

--- admin/claimer/admin_view_claims.php
+++ admin/claimer/admin_view_claims.php
@@ -176,6 +176,10 @@ $result = $conn->query($sql);
                             <form action='update_claim_status.php' method='POST' style='display:inline;'>
                                 <input type='hidden' name='claim_id' value='" . $row['id'] . "'>
                             </form>
+                            <form action='delete_claim.php' method='POST' style='display:inline;' onsubmit='return confirm(\"Are you sure you want to delete this claim?\");'>
+        <input type='hidden' name='claim_id' value='" . $row['id'] . "'>
+        <button type='submit' class='btn btn-danger btn-sm'>Delete</button>
+    </form>
                             <a href='https://ramonianlostgems.com/admin/claimer/claim_details.php?id=" . $row['id'] . "' class='btn btn-info btn-sm'>View</a>
                         </td>";
                         echo "</tr>";
@@ -184,13 +188,6 @@ $result = $conn->query($sql);
                 } else {
                     echo "<tr><td colspan='12' class='no-data'>No claims found.</td></tr>";
                 }
-                echo "<td>
-    <form action='delete_claim.php' method='POST' style='display:inline;' onsubmit='return confirm(\"Are you sure you want to delete this claim?\");'>
-        <input type='hidden' name='claim_id' value='" . $row['id'] . "'>
-        <button type='submit' class='btn btn-danger btn-sm'>Delete</button>
-    </form>
-    <a href='https://ramonianlostgems.com/admin/claimer/claim_details.php?id=" . $row['id'] . "' class='btn btn-info btn-sm'>View</a>
-</td>";
                 ?>
             </tbody>
         </table>
