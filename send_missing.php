ec8e1a2a1ebde1df595591e504004eb59146a5aa
darwinweng2002 <vdarwin860@gmail.com>
2024-09-26 23:01:28 +0800

    up

--- send_missing.php
+++ send_missing.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('config.php');
 session_start(); // Start session if not already started
 // Check if the user is logged in as either regular user or staff
