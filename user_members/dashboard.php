c465ee3ee7675a09f7edd736bc0ab2f279b32a9c
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 01:21:17 +0800

    up

--- user_members/dashboard.php
+++ user_members/dashboard.php
@@ -182,7 +182,7 @@ if (!$is_guest) {
         .swal2-overlay {
             overflow: auto;             /* Allow scrolling of the page if necessary */
         }
-        @media (max-width: 512spx) {
+        @media (max-width: 512px) {
         /* Adjust logo size */
         .logo img {
             max-height: 40px; /* Smaller logo for smaller screens */
