17adaf48d3e0310f7784e4c4408b5a36b43086b2
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 01:11:28 +0800

    up

--- user_members/dashboard.php
+++ user_members/dashboard.php
@@ -1,5 +1,9 @@
 <?php
 // Include the database configuration file
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include '../config.php';
 
 // Check if the user is logged in
