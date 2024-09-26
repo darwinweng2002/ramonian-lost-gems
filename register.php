2ce7531bb42a579b48042862f35238c123ebbbb9
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 03:04:41 +0800

    up

--- register.php
+++ register.php
@@ -281,9 +281,7 @@ body {
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <div class="back-btn-container">
+          <div class="back-btn-container">
     <button class="back-btn" onclick="history.back()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left">
             <line x1="19" y1="12" x2="5" y2="12"/>
@@ -292,6 +290,8 @@ body {
         Back
     </button>
 </div>
+        </div>
+      </section>
     </div>
     
   </main>
