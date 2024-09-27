755767c4c6dc5ee51f78bc698495839c50a494d8
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 18:05:20 +0800

    up

--- staff_login.php
+++ staff_login.php
@@ -223,13 +223,7 @@ if (isset($_POST['guest_login'])) {
                     data-size="large"
                     data-logo_alignment="left">
                   </div>
-                </div>
-              </div>
-          
-            </div>
-          </div>
-        </div>
-        <div class="back-btn-container">
+                  div class="back-btn-container">
     <button class="back-btn" onclick="history.back()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left">
             <line x1="19" y1="12" x2="5" y2="12"/>
@@ -238,6 +232,12 @@ if (isset($_POST['guest_login'])) {
         Back
     </button>
 </div>
+                </div>
+              </div>
+          
+            </div>
+          </div>
+        </div>
       </section>
     </div>
 
