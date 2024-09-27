656f12e9da49a0337bb4e0ae994ed2831119989e
darwinweng2002 <vdarwin860@gmail.com>
2024-09-27 18:15:40 +0800

    up

--- admin/login.php
+++ admin/login.php
@@ -118,7 +118,11 @@
                       <button class="btn btn-primary w-100" type="submit">Login</button>
 
                     </div>
-                    <div class="back-btn-container">
+                  </form>
+                  <br>
+                </div>
+              </div>
+              <div class="back-btn-container">
     <button class="back-btn" onclick="history.back()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left">
             <line x1="19" y1="12" x2="5" y2="12"/>
@@ -127,10 +131,6 @@
         Back
     </button>
 </div>
-                  </form>
-                  <br>
-                </div>
-              </div>
             </div>
           </div>
         </div>
