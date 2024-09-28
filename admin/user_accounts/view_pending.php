738d5603190eb1d5fb9f3aabdf8a3ba43f2a36bc
darwinweng2002 <vdarwin860@gmail.com>
2024-09-28 15:22:53 +0800

    up

--- admin/user_accounts/view_pending.php
+++ admin/user_accounts/view_pending.php
@@ -5,7 +5,6 @@ require '../../vendor/autoload.php';  // Correct the path to autoload.php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 include '../../config.php';
 
 // PHPMailer classes
