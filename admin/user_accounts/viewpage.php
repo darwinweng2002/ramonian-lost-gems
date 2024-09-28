f73061313644accb95bf0f678b5621c707323e3a
darwinweng2002 <vdarwin860@gmail.com>
2024-09-28 23:02:08 +0800

    up

--- admin/user_accounts/viewpage.php
+++ admin/user_accounts/viewpage.php
@@ -41,6 +41,7 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php require_once('../inc/header.php') ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Details</title>
