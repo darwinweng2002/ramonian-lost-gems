bc64136af268bd4add14b8366dd70953df35634f
darwinweng2002 <vdarwin860@gmail.com>
2024-09-28 13:54:08 +0800

    up

--- register.php
+++ register.php
@@ -241,6 +241,11 @@ body {
                           </select>
                           <div class="invalid-feedback">Please select your section.</div>
                       </div>
+                      <div class="col-12">
+                        <label for="school_id" class="form-label">School ID (JPG, PNG)</label>
+                        <input type="file" name="school_id" class="form-control" id="school_id" accept=".jpg,.jpeg,.png" required>
+                        <div class="invalid-feedback">Please upload your School ID (JPG or PNG).</div>
+                    </div>
                       <!-- Updated username field -->
                       <div class="col-12">
                       <label for="email" class="form-label">Username</label> 
