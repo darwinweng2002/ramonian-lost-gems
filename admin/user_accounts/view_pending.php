66082d2782d06263698ebc57ed4ac84cf1a5c04b
darwinweng2002 <vdarwin860@gmail.com>
2024-09-28 15:19:15 +0800

    up

--- admin/user_accounts/view_pending.php
+++ admin/user_accounts/view_pending.php
@@ -1,8 +1,8 @@
 <?php
+require '../../vendor/autoload.php';  // Update path to vendor/autoload.php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 include '../../config.php';
 
 // Database connection
