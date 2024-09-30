7816b112c6dc409918aa238611c3c0574c842f5e
darwinweng2002 <vdarwin860@gmail.com>
2024-10-01 01:00:22 +0800

    up

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -130,7 +130,7 @@
                             <circle cx="12" cy="12" r="10" />
                             <circle cx="12" cy="10" r="3" />
                             <path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" />
-                        </svg> Profile Staff</a></li>';
+                        </svg> Profile </a></li>';
             } elseif (isset($_SESSION['user_id'])) {
                 // If logged in as a regular user, show the user profile link
                 echo '<li><a href="https://ramonianlostgems.com/user_members/dashboard.php">
