6416a3586b97fb8ea1143828236dd5efff6cd63f
darwinweng2002 <vdarwin860@gmail.com>
2024-09-30 19:44:04 +0800

    addded placeholder

--- register.php
+++ register.php
@@ -285,7 +285,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 <div class="invalid-feedback">Please enter your first name.</div>
                             </div>
                             <div class="col-12">
-                                <label for="lastName" class="form-label">Last Name</label>
+                                <label for="lastName" placeholder="Please input your last name" class="form-label">Last Name</label>
                                 <input type="text" name="last_name" class="form-control" id="lastName" required>
                                 <div class="invalid-feedback">Please enter your last name.</div>
                             </div>
