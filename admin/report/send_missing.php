74f28986644077adb1e7046a80b250e328397222
darwinweng2002 <vdarwin860@gmail.com>
2024-10-04 00:22:57 +0800

    up

--- admin/report/send_missing.php
+++ admin/report/send_missing.php
@@ -87,7 +87,6 @@ if (isset($userId)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php require_once('../inc/header.php') ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post Missing Item</title>
