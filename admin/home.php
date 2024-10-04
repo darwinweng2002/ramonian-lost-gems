3eda06b260215eb08e621e0c35d12ebc79f5a984
darwinweng2002 <vdarwin860@gmail.com>
2024-10-04 11:54:14 +0800

    up

--- admin/home.php
+++ admin/home.php
@@ -97,6 +97,27 @@
           <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card info-card">
               <div class="card-body">
+                <h5 class="card-title"> Student User Accounts <span>| Pending</span></h5>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-dark bg-opacity-25 text-dark">
+                    <i class="bi bi-question-octagon"></i>
+                  </div>
+                  <div class="ps-3">
+                    <?php 
+                    $Items = $conn->query("SELECT * FROM `user_member` where `status` = 1")->num_rows;
+                    ?>
+                    <h6><?= format_num($Items) ?></h6>
+                    <!-- <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> -->
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
                 <h5 class="card-title">Reported Item <span>| Published</span></h5>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-primary bg-opacity-25 text-primary">
