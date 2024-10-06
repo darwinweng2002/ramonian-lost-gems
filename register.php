ee08e45a9c9dfaa6c21ced59fdae53a6d0349678
darwinweng2002 <vdarwin860@gmail.com>
2024-10-06 15:59:38 +0800

    up

--- register.php
+++ register.php
@@ -393,7 +393,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
           </div>
 
           <div class="back-btn-container">
-            <a href="your-link-here" class="back-btn">
+            <a href="https://ramonianlostgems.com/" class="back-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left">
                     <line x1="19" y1="12" x2="5" y2="12"/>
                     <polyline points="12 19 5 12 12 5"/>
@@ -401,8 +401,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 Cancel
             </a>
         </div>
-
-
         </div>
       </section>
     </div>
