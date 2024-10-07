1fc287505b122da1599ffa4936f81cc1a69fd49a
darwinweng2002 <vdarwin860@gmail.com>
2024-10-07 18:08:01 +0800

    up

--- admin/messages/view_reported_item.php
+++ admin/messages/view_reported_item.php
@@ -1,8 +1,10 @@
+
 <?php
-// Enable error reporting for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+// Enable error reporting for debugging
+
 include '../../config.php';
 
 // Database connection
