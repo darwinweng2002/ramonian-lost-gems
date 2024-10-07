407a65ec3a25d20750e9e95d98f9b57deaba5f6e
darwinweng2002 <vdarwin860@gmail.com>
2024-10-07 14:11:32 +0800

    up

--- send_message.php
+++ send_message.php
@@ -1,7 +1,7 @@
 <?php
 include('config.php');
 
-session_start(); // Make sure the session is started
+session_start(); // Start session if not already started
 
 // Check if the user is logged in as either regular user or staff
 if (!isset($_SESSION['user_id']) && !isset($_SESSION['staff_id'])) {
