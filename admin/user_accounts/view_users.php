fbfca64beb7ccb36caad8e27efef88f21fb0bf2c
darwinweng2002 <vdarwin860@gmail.com>
2024-10-08 14:38:21 +0800

    up

--- admin/user_accounts/view_users.php
+++ admin/user_accounts/view_users.php
@@ -6,7 +6,8 @@ include '../../config.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'vendor/composer/autoload_file.php';
+require '../vendor/autoload.php';
+
 
 // Database connection
 $conn = new mysqli("localhost", "u450897284_root", "Lfisgemsdb1234", "u450897284_lfis_db");
