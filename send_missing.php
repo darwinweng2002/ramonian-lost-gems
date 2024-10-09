3549d03fb1323115a94f59971dec14c337432524
darwinweng2002 <vdarwin860@gmail.com>
2024-10-09 19:12:19 +0800

    up

--- send_missing.php
+++ send_missing.php
@@ -2,7 +2,7 @@
 include('config.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_A)
+error_reporting(E_ALL);
 session_start(); // Start session if not already started
 
 if (!isset($_SESSION['user_id']) && !isset($_SESSION['staff_id'])) {
