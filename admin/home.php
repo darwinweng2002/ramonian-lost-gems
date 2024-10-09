a118390c8f3ad27803bc8134c72a48579bf5a0e7
darwinweng2002 <vdarwin860@gmail.com>
2024-10-10 01:17:22 +0800

    up

--- admin/home.php
+++ admin/home.php
@@ -26,6 +26,23 @@
               </div>
 
             </div>
+          </div> <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title">Missing Items <span>| Pending</span></h5>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="ps-3">
+                    <?php 
+                    $Items = $conn->query("SELECT * FROM `missing_items` where `status` = 0")->num_rows;
+                    ?>
+                    <h6><?= format_num($Items) ?></h6>
+                    <!-- <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> -->
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
           <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card info-card">
@@ -85,6 +102,24 @@
             <div class="card info-card">
               <div class="card-body">
                 <h5 class="card-title">Reported Item <span>| Published</span></h5>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="ps-3">
+                    <?php 
+                    $items = $conn->query("SELECT * FROM `message_history` where `status` = 1")->num_rows;
+                    ?>
+                    <h6><?= format_num($items) ?></h6>
+                    <!-- <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> -->
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title">Reported Found Item <span>| Pending</span></h5>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="ps-3">
                     <?php 
