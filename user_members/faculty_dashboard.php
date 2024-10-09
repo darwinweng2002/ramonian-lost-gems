f64fe5cdfebb7650b33acd76ec58438bb89962bf
darwinweng2002 <vdarwin860@gmail.com>
2024-10-09 13:46:50 +0800

    up

--- user_members/faculty_dashboard.php
+++ user_members/faculty_dashboard.php
@@ -380,10 +380,12 @@ $is_non_teaching = empty($department);
                                         <div class="d-flex justify-content-center">
                                             <div class="text-center mb-3">
                                             <?php if ($avatar): ?>
-                                                <img src="../uploads/avatars/<?= htmlspecialchars($avatar) ?>" alt="User Avatar" style="width: 100px; height: 100px; border-radius: 50%;">
-                                            <?php else: ?>
-                                                <img src="../uploads/avatars/default-avatar.png" alt="Default Avatar" style="width: 100px; height: 100px; border-radius: 50%;">
-                                            <?php endif; ?>
+                                            <img src="../uploads/avatars/<?= htmlspecialchars($avatar) ?>" alt="User Avatar" style="width: 100px; height: 100px; border-radius: 50%;">
+                                        <?php else: ?>
+                                            <div style="width: 100px; height: 100px; border-radius: 50%; background-color: #ddd; display: flex; align-items: center; justify-content: center; font-size: 14px; color: #555;">
+                                                No Avatar
+                                            </div>
+                                        <?php endif; ?>
                                             </div>
                                         </div>
                                         <p class="text-center small">Welcome, <?= htmlspecialchars($first_name ?? '') . ' ' . htmlspecialchars($last_name ?? '') ?></p>
