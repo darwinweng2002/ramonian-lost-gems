27b301d98978ad3ce2ad11d8c4011c303cc724b0
darwinweng2002 <vdarwin860@gmail.com>
2024-10-09 15:50:44 +0800

    up

--- itemss/view_missing.php
+++ itemss/view_missing.php
@@ -1,5 +1,8 @@
 <?php
 include '../config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id']) && !isset($_SESSION['staff_id'])) {
