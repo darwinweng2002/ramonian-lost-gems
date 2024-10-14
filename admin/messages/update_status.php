5aee63783fb5fbad2729e71ac0793b3f26784a11
darwinweng2002 <vdarwin860@gmail.com>
2024-10-14 16:10:14 +0800

    up

--- admin/messages/update_status.php
+++ admin/messages/update_status.php
@@ -14,8 +14,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $itemId = intval($_POST['id']);
     $newStatus = intval($_POST['status']);
     
-    // Check that the status is valid (between 0 and 3)
-    if ($newStatus >= 0 && $newStatus <= 3) {
+    // Check that the status is valid (between 0 and 4)
+    if ($newStatus >= 0 && $newStatus <= 4) { // Allow status '4' for Denied
         // Update the status in the database
         $stmt = $conn->prepare("UPDATE message_history SET status = ? WHERE id = ?");
         $stmt->bind_param('ii', $newStatus, $itemId);
