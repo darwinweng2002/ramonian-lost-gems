2b699215bdd0caa481ec18ee182406b356816367
darwinweng2002 <vdarwin860@gmail.com>
2024-10-15 18:46:56 +0800

    up

--- admin/inc/navigation.php
+++ admin/inc/navigation.php
@@ -70,6 +70,29 @@
         <path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
         <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
       </svg>
+      <span>Denied Item Reports</span>
+
+      <?php 
+      // Fetch the count of pending accounts where status is set to 'pending' (status = 'pending')
+      $pending_accounts = $conn->query("SELECT COUNT(*) AS count FROM `message_history` WHERE `status` = 'denied'")->fetch_assoc();
+      ?>
+      
+      <!-- Check if there are any pending accounts -->
+      <?php if($pending_accounts['count'] > 0): ?>
+        <span class="badge rounded-pill bg-danger text-light ms-4">
+          <?= $pending_accounts['count'] ?>  <!-- Display pending accounts count -->
+        </span>
+      <?php endif; ?>
+    </a>
+</li>
+  <li class="nav-item">
+    <a class="nav-link <?= $page != 'user/list' ? 'collapsed' : '' ?> nav-users" href="https://ramonianlostgems.com/admin/user_accounts/view_users.php">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 30 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users">
+        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/>
+        <circle cx="9" cy="7" r="4"/>
+        <path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
+        <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+      </svg>
       <span>Pending Student Accounts</span>
 
       <?php 
