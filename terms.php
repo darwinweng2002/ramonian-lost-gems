6b7f48efdb624e76e9ca16acff7f440de69440e4
darwinweng2002 <vdarwin860@gmail.com>
2024-10-16 20:25:09 +0800

    up

--- terms.php
+++ terms.php
@@ -122,6 +122,7 @@ include 'config.php';
               
               <div class="d-flex justify-content-center py-4">
                 <a href="#" class="logo d-flex align-items-center w-auto">
+                  <img src="<?= validate_image($_settings->info('logo')) ?>" alt="">
                   <img src="uploads/logo.png" alt="">
                   <br>
                   <span><?= $_settings->info('name') ?></span>
