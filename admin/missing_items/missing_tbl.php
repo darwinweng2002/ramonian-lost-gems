1bfccac873509c1b8e7a2f6a2342d410e661681c
darwinweng2002 <vdarwin860@gmail.com>
2024-10-29 21:42:30 +0800

    up

--- admin/missing_items/missing_tbl.php
+++ admin/missing_items/missing_tbl.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include '../../config.php';
 
 // Define the base path where the images are stored
