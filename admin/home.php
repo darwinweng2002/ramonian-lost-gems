f4f4def8b32e4e4cb9751c0faf3ebd8909761031
darwinweng2002 <vdarwin860@gmail.com>
2024-11-02 13:01:58 +0800

    up

--- admin/home.php
+++ admin/home.php
@@ -65,7 +65,7 @@
           <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card info-card">
               <div class="card-body">
-                <h5 class="card-title"> Student User Accounts <span>| Active</span></h5>
+                <h5 class="card-title">User Accounts <span>| Active</span></h5>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="ps-3">
                     <?php 
@@ -80,7 +80,7 @@
 
             </div>
           </div>
-          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+         <!-- <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card info-card">
               <div class="card-body">
                 <h5 class="card-title"> Employee Accounts <span>| Pending</span></h5>
@@ -90,18 +90,18 @@
                     $Items = $conn->query("SELECT * FROM `user_staff` where `status` = 'pending'")->num_rows;
                     ?>
                     <h6><?= format_num($Items) ?></h6>
-                    <!-- <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> -->
+                     <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> 
 
                   </div>
                 </div>
               </div>
 
             </div>
-          </div>
+          </div> -->
           <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card info-card">
               <div class="card-body">
-                <h5 class="card-title"> Student User Accounts <span>| Pending</span></h5>
+                <h5 class="card-title">User Accounts <span>| Pending</span></h5>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="ps-3">
                     <?php 
