7b64cbe64ec274fc81db9697a0ff842babf1c63e
darwinweng2002 <vdarwin860@gmail.com>
2024-12-05 15:09:55 +0800

    up

--- config.php
+++ config.php
@@ -2,6 +2,9 @@
 ob_start();
 ini_set('date.timezone','Asia/Manila');
 date_default_timezone_set('Asia/Manila');
+$dateTime = new DateTime("now", new DateTimeZone('Asia/Manila')); // Adjust timezone
+echo $dateTime->format("h:i A"); // Display in 12-hour format
+
 session_start();
 
 require_once('initialize.php');
